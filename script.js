40f3e9c86d03ab0de37389e48e1658331a365f44
cadcock15 <5466552+cadcock15@users.noreply.github.com>
2023-05-08 21:35:41 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * The Odin Project
+ * Project name
+ * @author: Christopher Adcock
+ */
+
+/* ==========================================================================
+   Section
+   ========================================================================== */
+
+/* Sub-Section
+   ========================================================================== */
+
+/**
+ * Function Comment and explantation
+ *
+ * @TODO: Delete this function 
+ * @param {number} x explanation of input value
+ * @return {number} explantaion of return value
+ */
+//function myFunc(x) {
+//  return x;
+//}
